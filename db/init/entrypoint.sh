3223a29574a02a06afe44580a0a9a31954017370
amrita112 <amsi.1994@gmail.com>
2025-04-09 16:00:46 -0400

    delete all non-essential files

--- db/init/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Substitute environment variables in the SQL script
-envsubst < /docker-entrypoint-initdb.d/init.sql > /docker-entrypoint-initdb.d/init_processed.sql
-
-# Run the processed SQL script
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/init_processed.sql
